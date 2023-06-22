f244756a8a1b47a9f060f6b397034b4a04594bf2
suxa <suxa5637@mail.ru>
2023-06-22 23:46:30 +0500

    check phone duplicate, prevent creation

--- pkg/repository/phoneRepo.go
+++ pkg/repository/phoneRepo.go
@@ -3,6 +3,7 @@ package repository
 import (
 	Sarkor_test "Sarkor-test"
 	"database/sql"
+	"errors"
 	"fmt"
 )
 
@@ -17,14 +18,25 @@ func NewPhoneRepoImpl(db *sql.DB) *PhoneRepoImpl {
 // Create new phone
 // Return phone id, error
 func (p *PhoneRepoImpl) CreatePhone(phone Sarkor_test.Phone) (int, error) {
-	var id int
-	// Create query
+	var id = Sarkor_test.UNDEFINED_ID
+	// Check phone duplicate
 	query := fmt.Sprintf(
+		"SELECT id FROM %s WHERE phone=$1",
+		PhoneTable)
+	row := p.db.QueryRow(query, phone.Phone)
+	if err := row.Scan(&id); err != nil && id != Sarkor_test.UNDEFINED_ID {
+		return id, err
+	} else if id != Sarkor_test.UNDEFINED_ID {
+		return id, errors.New("phone duplicate")
+	}
+
+	//Create query
+	query = fmt.Sprintf(
 		"INSERT INTO %s (phone, description, isFax, userId) VALUES ($1, $2, $3, $4) RETURNING id",
 		PhoneTable)
 
-	// Fill query and execute
-	row := p.db.QueryRow(query, phone.Phone, phone.Description, phone.IsFax, phone.UserId)
+	//Fill query and execute
+	row = p.db.QueryRow(query, phone.Phone, phone.Description, phone.IsFax, phone.UserId)
 	if err := row.Scan(&id); err != nil {
 		return Sarkor_test.UNDEFINED_ID, err
 	}

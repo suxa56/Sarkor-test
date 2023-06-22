4f75acc6a76839cf6db555400561d9442c075c7b
suxa <suxa5637@mail.ru>
2023-06-22 09:36:24 +0500

    config with viper

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,15 +5,26 @@ import (
 	"Sarkor-test/pkg/handler"
 	"Sarkor-test/pkg/repository"
 	"Sarkor-test/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(Sarkor_test.Server)
-	if err := srv.Run("9090", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
